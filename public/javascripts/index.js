a1f66f0507bfc12d499c819a647aac3455593d39
qiaojm <179888548@qq.com>
2017-03-30 10:42:57 +0800

    modify index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -30,17 +30,6 @@ xhr.onlode = function(){
       },function(e){console.log(err);});
 
   }
-
-// xhr.onlode = function(){
-// 	audioCtx.decodeAudioData(xhr.response, function(buffer) {
-// 		console.log(xhr.response);
-// 		var source = audioCtx.createBufferSource();
-//         source.buffer = buffer;
-//         source.connect(audioCtx.destination);
-//         source[source.start?"start":"noteOn"](0);
-//       },function(e){console.log(err);});
-
-//   }
 xhr.send();
 }
 
