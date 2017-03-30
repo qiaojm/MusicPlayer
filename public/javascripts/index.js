efa5488e210a5b47be71359e1a5c1c6e2d36cadd
qiaojm <179888548@qq.com>
2017-03-30 18:17:42 +0800

    add gainNode and modify bug about play

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -18,19 +18,36 @@ for (var i = 0; i < lis.length; i++) {
 var xhr = new XMLHttpRequest();
 //通过AudioContext来解码歌曲并播放
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+//创建一个改变音量的对象
+var gainNode = audioCtx[audioCtx.createGain?"createGain":"createGainNode"]() ;
+//判断是否已有歌曲播放，切换歌曲，防止多个歌曲同时播放
+var judgeSource = null;
+//若前一首歌曲还未解码，这时仍然会同时播放多首歌曲
+var count = 0;
 function load(url) {
+	var n = ++count;
+    console.log(n,count);
+    xhr.abort();//取消当前响应，关闭连接并且结束任何未决的网络活动。
     xhr.open("GET", url);
     xhr.responseType = "arraybuffer";
     xhr.onload = function() {
+    	if(n != count) return;
+    	//判断是否已有歌曲播放
+    	judgeSource && judgeSource[judgeSource.stop?"stop":"noteOff"]();
         // 旧版的回调函数语法
-        audioCtx.decodeAudioData(xhr.response, function(buffer) { // https://developer.mozilla.org/zh-CN/docs/Web/API/AudioContext/decodeAudioData
-            var source = audioCtx.createBufferSource(); // https://developer.mozilla.org/zh-CN/docs/Web/API/AudioContext/createBufferSource
+        audioCtx.decodeAudioData(xhr.response, function(buffer) {
+            //创建一个控制歌曲播放的对象 
+    	if(n != count) return;           
+            console.log(n,count);
+        	var source = audioCtx.createBufferSource();
             source.buffer = buffer;
-            // connect the AudioBufferSourceNode to the
-            // destination so we can hear the sound
-            source.connect(audioCtx.destination);
-            // start the source playing
-            source.start(); // https://developer.mozilla.org/zh-CN/docs/Web/API/AudioBufferSourceNode
+            source.connect(gainNode);
+            gainNode.connect(audioCtx.destination);
+            //设置循环
+            source.loop = true;
+            //开始播放
+            source[source.start?"start":"noteOn"](0); 
+            judgeSource = source;
         }, function(error) {
             console.log(error);
         });
@@ -52,6 +69,13 @@ function load(url) {
     }
     xhr.send();
 }
+function changeVolume(percent){
+	gainNode.gain.value = percent*percent ;
+}
+$("#volume")[0].onchange = function (){
+  changeVolume(this.value/this.max);
+}
+$("#volume")[0].onchange();
 //发送一个http请求2
 // function load(url){
 // 	 xhr=null;
